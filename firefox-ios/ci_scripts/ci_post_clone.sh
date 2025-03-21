582cc67fc28964c591a07bedc06304f28645a528
CS <1024@qq.com>
2025-03-21 13:15:41 +0800

    支持xcode cloud

---
+++ firefox-ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+echo "开始安装cocoapods"
+brew install cocoapods
+echo "cocoapods安装完毕"
+
+echo "开始设置cocoapods"
+pod setup
+echo "cocoapods设置完毕"
+
+echo "开始安装pods依赖库"
+pod install
+echo "pods依赖库安装完毕"
+
+echo "Applications相关"
+
+ls /Applications
+
+echo "brew list相关"
+brew list
+
+echo "env相关"
+env
+
+echo "PATH相关"
+echo $PATH
