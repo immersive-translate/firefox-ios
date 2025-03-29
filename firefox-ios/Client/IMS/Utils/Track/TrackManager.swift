0c3e7b85059cd06df6573597a8fd8e093eaed80a
CS <1024@qq.com>
2025-03-29 14:21:58 +0800

    参数

--- firefox-ios/Client/IMS/Utils/Track/TrackManager.swift
+++ firefox-ios/Client/IMS/Utils/Track/TrackManager.swift
@@ -16,20 +16,20 @@ final class TrackManager {
 }
 
 extension TrackManager {
-    func event(_ name: String, param: [String: Any]? = nil) {
-        sendEvent(name: name, param: param)
+    func event(_ name: String, params: [String: Any]? = nil) {
+        sendEvent(name: name, params: params)
     }
 }
 
 extension TrackManager {
-    private func sendEvent(name: String, param: [String: Any]? = nil) {
+    private func sendEvent(name: String, params: [String: Any]? = nil) {
         guard let url = URL(string: "\(IMSAppUrlConfig.analyticsURL)/collect") else {
             return
         }
         let parameters: [String: Any] = [
             "nonce": UUID().uuidString,
             "subject": "user_behaviour",
-            "logs": [buildLog(eventName: name, param: param)]
+            "logs": [buildLog(eventName: name, params: params)]
         ]
         AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
             .response { response in
@@ -44,8 +44,8 @@ extension TrackManager {
             }
     }
     
-    private func buildLog(eventName: String, param: [String: Any]?) -> String {
-        let dict = [
+    private func buildLog(eventName: String, params: [String: Any]?) -> String {
+        var dict: [String: Any] = [
             "os_name": UIDevice.current.systemName,
             "os_version": UIDevice.current.systemVersion,
             "os_version_name": UIDevice.current.systemVersion,
@@ -53,12 +53,33 @@ extension TrackManager {
             "platform_type": getUserInterfaceIdiomName(),
             "version": AppInfo.appVersion,
             "event_name": eventName,
-            "device_id": Adjust.adid(),
+            "device_id": Adjust.adid() as Any,
         ]
+        if let params = params {
+            dict.merge(processParams(params)) { (_, new) in new }
+        }
         return JSON(dict).rawString([:]) ?? ""
     }
     
-    func getUserInterfaceIdiomName() -> String {
+    private func processParams(_ input: [String: Any]) -> [String: Any] {
+        var result: [String: Any] = [:]
+        var intIndex = 1
+        var charIndex = 1
+        
+        for value in input.values {
+            if let intValue = value as? Int {
+                result["ex_int_arg\(intIndex)"] = intValue
+                intIndex += 1
+            } else {
+                result["ex_char_arg\(charIndex)"] = "\(value)"
+                charIndex += 1
+            }
+        }
+        
+        return result
+    }
+    
+    private func getUserInterfaceIdiomName() -> String {
         switch UIDevice.current.userInterfaceIdiom {
         case .phone:
             return "iPhone"
