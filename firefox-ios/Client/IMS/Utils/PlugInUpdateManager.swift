c918a4374ccaf0fceb6db514523ec479f475d168
CS <cs@1024.com>
2025-11-19 22:08:58 +0800

    更新js

--- firefox-ios/Client/IMS/Utils/PlugInUpdateManager.swift
+++ firefox-ios/Client/IMS/Utils/PlugInUpdateManager.swift
@@ -17,7 +17,7 @@ class PlugInUpdateManager {
     private let cacheDirectory = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)[0];
     private let jsBundleUrlString = Bundle.main.path(
         forResource: "immersive-translate.user", ofType: "js")!
-    private let jsBundleEtag = ##"W/"cc8b0c070bd052cdd52dcd98a3e703c6""##
+    private let jsBundleEtag = ##"W/"c03f9a37d150d2fb210a7cac42fc1115""##
     
     private var isFetching = false;
     
